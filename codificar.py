e1987974f42dfddda5e991c152925873f6f92f7e
Carlos Guzmán Ramírez <cguzmanr17@gmail.com>
2022-05-04 09:38:41 -0600

    Finalización del Proyecto
    Se carga la información final de la entrega del proyecto

--- codificar.py
+++ codificar.py
@@ -1,7 +1,7 @@
 #######################################################
 #Creado por: Carlos Guzmán, Samuel Garcés
 #Fecha de creación: 4/4/2022 7:00 pm
-#Última modificación: 27/4/2022 10:30 am 
+#Última modificación: 26/4/2022 8:00 pm 
 #Versión de python: 3.10.2
 #######################################################
 
@@ -51,31 +51,7 @@ def validarOpcion(opcion, tope): # Esta función permite validar las opciones en
     else: 
         print("Debe ingresar una opción valida")
         return False        
-def validarNumero(pnum, numeroMinimo): # Permite validar unicamente valores numéricos mayores a un número definido, y con máximo infinito
-    """
-    Funcionamiento: Validar las entradas numericas
-    Entradas: pnum (str) número a trabajar, numeroMinimo (int) parametro que define el número minimo con el que desea trabajar
-    Salidas: (Booleano) realimentar al usuario con la corrección de posibles errores o permitir el avance del proceso.
-    """
-    if re.match("^\d+$", pnum): # Opción correcta
-        if int(pnum)>=numeroMinimo: 
-            return True
-        else:
-            print("Debe ingresar un número mayor o igual que "+str(numeroMinimo))
-            return False
-    elif pnum.find(" ") != -1:    # Valida en caso de ingresar espacios en blanco entre los dígitos             
-        print ("No debe digitar espacios")
-        return False
-    elif pnum == "":    # Valida en caso de no ingresar ningún valor                                     
-        print("Debe ingresar un valor numérico")
-        return False
-    elif re.match("^\D+$", pnum): # Valida en caso de ingresar un valor no-numérico
-        print("El valor debe de ser un número entero")
-        return False
-    else: # Valida cualquier otro error
-        print("Valor inválido, inténtelo nuevamente")
-        return False
-def validarNumero2Digitos(pnum, numeroMinimo, numeroMaximo): # Permite validar valores numéricos mayores a un número definido, y con máximo definido
+def validarNumero(pnum, numeroMinimo, numeroMaximo): # Permite validar valores numéricos mayores a un número definido, y con máximo definido
     """
     Funcionamiento: Validar las entradas numericas
     Entradas: pnum (str) número a trabajar, numeroMinimo (int) parametro que define el número minimo con el que desea trabajar
@@ -286,7 +262,7 @@ def obtenerCodVigenere(accion):
     if validarFrase(frase)==False:
         return obtenerCodVigenere(accion)
     cifra = input("Por favor, ingrese la cifra: ")
-    if validarNumero2Digitos(cifra,10,99)==False:
+    if validarNumero(cifra,10,99)==False:
         return obtenerCodVigenere(accion)
     if accion == "codificar":
         print(procesarCodVigenere(frase, cifra)) 
@@ -577,7 +553,7 @@ def elegirAccion(ejercicio):
     Entradas: ejercicio (str) nombre del ejercicio al que pertenece la acción, accion (str) acción que desea realizar
     Salidas: resultado del proceso
     """
-    accion = input(f"___________________________\n¿Que acción desea realizar? - {ejercicio} \n1- Codificar     2- Decodificar?     0- Regresar al menú\n>>> ")
+    accion = input(f"___________________________\n¿Que acción desea realizar? - {ejercicio} \n1- Codificar     2- Decodificar     0- Regresar al menú\n>>> ")
     if validarOpcion(accion, 2):
         if accion == "0":
             return 0
@@ -611,7 +587,7 @@ def menu(): ### Menú principal
         if validarOpcion(opcion, 8)==False:  # Validación de las opciones
             return menu()
         elif opcion == "0":  # Finaliza el proceso
-            return "\n---Ejecución finalizada---"
+            return "\n---Ejecución finalizada---\n"
         else:   # Selecciona opciones del menú
             if opcion == "1":
                 ejercicio = "Cifrado César"
